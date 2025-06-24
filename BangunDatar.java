b61cd65223340dd383b9c932c3b03c14e662c9c3
Bintang24-startup <sudrajatbintang0@gmail.com>
2025-06-24 16:43:06 +0800

    initial commit

---
+++ BangunDatar.java
@@ -0,0 +1,11 @@
+package latihan3susulan;
+
+/*
+    Nama  : Sudrajat Bintang Nur Aflah
+    NPM   : 23010010615
+    Kelas : TI-4A
+*/
+
+public class BangunDatar {
+    // Isi class BangunDatar
+}
